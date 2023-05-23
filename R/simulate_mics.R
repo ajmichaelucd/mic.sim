4a34247481329d5c66617764699019124be022d1
Alec Michael <alecmichael@Alecs-MacBook-Pro-2.local>
2023-05-23 12:44:02 -0700

    small change to simulate mics since the t_dist went outside bounds if paired with the t_dist i often have saved in the environment

--- R/simulate_mics.R
+++ R/simulate_mics.R
@@ -34,7 +34,7 @@
 #' @examples
 simulate_mics <- function(n = 100,
                           t_dist = function(n){runif(n, min = 0, max = 1)},
-                          pi = function(t) {z <- 0.5 + 0.2 * t
+                          pi = function(t) {z <- 0.5 + 0.002 * t
                           c("1" = z, "2" = 1- z)},
                           `E[X|T,C]` = function(t, c)
                           {
