3636bdccc693a7a4f8f4ac9d9718866021c6b665
ajmichaelucd <58995023+ajmichaelucd@users.noreply.github.com>
2024-06-18 13:41:05 -0700

    swap in new fit_surv_EM

--- R/fit_surv_EM.R
+++ R/fit_surv_EM.R
@@ -1,64 +1,107 @@
 #' Title
 #'
-#' @param random_seeds_vector
+#' @param pre_set_degrees
+#' @param max_degree
+#' @param degree_sets
 #' @param visible_data
-#' @param mu_formula
+#' @param nfolds
+#' @param non_linear_term
+#' @param covariates
 #' @param pi_formula
 #' @param max_it
 #' @param ncomp
 #' @param tol_ll
 #' @param pi_link
 #' @param verbose
-#' @param initial_weighting
 #' @param model_coefficient_tolerance
-#' @param maxiter_survreg
+#' @param initial_weighting
 #' @param sd_initial
-#' @param randomize
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fit_surv_EM =
-  function(
-    visible_data,
-    mu_formula = Surv(time = left_bound,
-                      time2 = right_bound,
-                      type = "interval2") ~ pspline(t, df = 0, caic = TRUE),
-    pi_formula = c == "2" ~ s(t),
-    max_it = 500,
-    ncomp = 2,
-    tol_ll = 1e-6,
-    pi_link = "logit",
-    verbose = 1,
-    initial_weighting = 9,
-    model_coefficient_tolerance = 0.00001,
-    maxiter_survreg = 30,
-    sd_initial = 0.2,
-    non_linear_term = "t",
-    covariates = NULL) {
-    EM_algorithm(
+fit_surv_EM = function(pre_set_degrees = NULL, #c(7,7)
+                       max_degree,
+                       degree_sets = "matched",
+                       #"independent"
+                       visible_data,
+                       nfolds,
+                       non_linear_term = "t",
+                       covariates = NULL,
+                       pi_formula = c == "2" ~ s(t),
+                       max_it = 3000,
+                       ncomp = 2,
+                       tol_ll = 1e-6,
+                       pi_link = "logit",
+                       verbose = 3,
+                       model_coefficient_tolerance = 0.00001,
+                       initial_weighting = 9,
+                       sd_initial = 0.2) {
+  if (is.null(pre_set_degrees)) {
+    cv_results = full_surv_cv(
+      max_degree = max_degree,
+      degree_sets = degree_sets,
       visible_data = visible_data,
-      model = "surv",
-      mu_formula = mu_formula,
+      nfolds = nfolds,
+      non_linear_term = non_linear_term,
+      covariates = covariates,
       pi_formula = pi_formula,
       max_it = max_it,
       ncomp = ncomp,
       tol_ll = tol_ll,
-      browse_at_end = FALSE,
-      browse_each_step = FALSE,
-      plot_visuals = FALSE,
-      prior_step_plot = FALSE,
-      pause_on_likelihood_drop = FALSE,
       pi_link = pi_link,
       verbose = verbose,
       model_coefficient_tolerance = model_coefficient_tolerance,
-      maxiter_survreg = maxiter_survreg,
       initial_weighting = initial_weighting,
-      sd_initial = sd_initial,
-      stop_on_likelihood_drop = FALSE,
-      non_linear_term = non_linear_term,
-      covariates = covariates
-    ) %>% return()
-    ##save linear model in step 9
+      sd_initial = sd_initial
+    ) %>%
+      summarize(
+        .by = c(degree_1, degree_2),
+        log_likelihood = sum(fold_likelihood)
+      ) %>%
+      arrange(desc(log_likelihood))
+
+    mu_formula = write_all_surv_formulas(non_linear_term,
+                                         pull_top_degree_set(cv_results),
+                                         covariates)
+  } else{
+    if(any(pre_set_degrees == 1)){
+      errorCondition("degrees of freedom for pspline in surv package must be greater than 2")
+    }
+    mu_formula = write_all_surv_formulas(non_linear_term, pre_set_degrees, covariates)
+    cv_results = NULL
   }
+  output = EM_algorithm(
+    visible_data = visible_data,
+    model = "surv",
+    #"mgcv"
+    mu_formula = mu_formula,
+    #mu_formula = yi ~ s(t),
+    pi_formula = pi_formula,
+    #or: c == "2" ~ lo(t)
+    max_it = max_it,
+    ncomp = ncomp,
+    tol_ll = tol_ll,
+    browse_at_end = FALSE,
+    browse_each_step = FALSE,
+    plot_visuals = FALSE,
+    prior_step_plot = FALSE,
+    pause_on_likelihood_drop = FALSE,
+    pi_link = pi_link,
+    verbose = verbose,
+    model_coefficient_tolerance = model_coefficient_tolerance,
+    maxiter_survreg = 30,
+    initial_weighting = initial_weighting,
+    sd_initial = sd_initial,
+    stop_on_likelihood_drop = FALSE,
+    n_models = 100,
+    seed = NULL,
+    randomize = "all"
+  )
+
+  output$cv_results = cv_results
+
+  output %>% return()
+
+}
