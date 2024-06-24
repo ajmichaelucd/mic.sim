e05a3a72ded4a78d407f18d81bb661a15d65869c
ajmichaelucd <58995023+ajmichaelucd@users.noreply.github.com>
2024-06-24 14:35:02 -0700

    fixed an issue with debug kicking issue with reduced model

--- misc/fit_em_draft_combined_v2.R
+++ misc/fit_em_draft_combined_v2.R
@@ -50,7 +50,8 @@ fit_EM = function(model = "surv", #"polynomial",
         cv_results_intermediate %>%
         summarize(
         .by = starts_with("degree"),
-        log_likelihood = sum(fold_likelihood)
+        log_likelihood = sum(fold_likelihood),
+        total_repeats = sum(repeats)
       ) %>%
       arrange(desc(log_likelihood))
 
@@ -182,36 +183,99 @@ full_cv = function(
 }
 
 
-
-
 single_cv_all = function(model = "surv",
-                          approach = "full",
-    degrees,
-                          visible_data,
-                          nfolds,
-                          non_linear_term = "t",
-                          covariates = NULL,
-                          pi_formula = c == "2" ~ s(t),
-    fixed_side = NULL,
-    extra_row = FALSE,
-                          max_it = 3000,
-                          ncomp = 2,
-                          tol_ll = 1e-6,
-                          pi_link = "logit",
-                          verbose = 3,
-                          model_coefficient_tolerance = 0.00001,
-    maxiter_survreg = 30,
-                          initial_weighting = 9,
-                          sd_initial = 0.2,
-                          scale = NULL) {
-  message("CV for degrees", degrees)
+                         approach = "full",
+                         degrees,
+                         visible_data,
+                         nfolds,
+                         non_linear_term = "t",
+                         covariates = NULL,
+                         pi_formula = c == "2" ~ s(t),
+                         fixed_side = NULL,
+                         extra_row = FALSE,
+                         max_it = 3000,
+                         ncomp = 2,
+                         tol_ll = 1e-6,
+                         pi_link = "logit",
+                         verbose = 3,
+                         model_coefficient_tolerance = 0.00001,
+                         maxiter_survreg = 30,
+                         initial_weighting = 9,
+                         sd_initial = 0.2,
+                         scale = NULL,
+                         reruns_allowed = 3) {
+  for(i in 1:reruns_allowed){
+    message("CV for degrees", degrees, "; attempt", i)
 
-  fold_output = tibble(fold_likelihood = map_dbl(
-    1:nfolds,
-    ~ get_fold_likelihood_all(
+    fold_output = tibble(fold_likelihood = map_dbl(
+      1:nfolds,
+      ~ get_fold_likelihood_all_safe_single_output(
+        model = model,
+        approach = approach,
+        .x,
+        visible_data = assign_folds(visible_data, nfolds),
+        degrees,
+        non_linear_term = non_linear_term,
+        covariates = covariates,
+        pi_formula = pi_formula,
+        fixed_side = fixed_side,
+        extra_row = extra_row,
+        max_it = max_it,
+        ncomp = ncomp,
+        tol_ll = tol_ll,
+        pi_link = pi_link,
+        verbose = verbose,
+        model_coefficient_tolerance = model_coefficient_tolerance,
+        maxiter_survreg = maxiter_survreg,
+        initial_weighting = initial_weighting,
+        sd_initial = sd_initial,
+        scale = scale
+      )
+    ))
+
+    rep = i - 1
+
+    if((fold_output %>% filter(is.nan(fold_likelihood)) %>% nrow()) == 0){
+      break
+    }
+
+  }
+
+  tibble(fold_output, add_all_degrees_to_fold_likelihood(fold_output, degrees, nfolds)) %>%
+    mutate(repeats = rep) %>%
+    return()
+}
+
+
+get_fold_likelihood_all_safe = safely(get_fold_likelihood_all, otherwise = NaN)
+
+
+get_fold_likelihood_all_safe_single_output =
+  function(model = "surv",
+           approach = "full",
+           i,
+           visible_data,
+           degrees,
+           non_linear_term = "t",
+           covariates = NULL,
+           pi_formula = c == "2" ~ s(t),
+           fixed_side = NULL,
+           extra_row = FALSE,
+           max_it = 3000,
+           ncomp = 2,
+           tol_ll = 1e-6,
+           pi_link = "logit",
+           verbose = 3,
+           model_coefficient_tolerance = 0.00001,
+           maxiter_survreg = 30,
+           initial_weighting = 9,
+           sd_initial = 0.2,
+           scale = NULL){
+
+    out = get_fold_likelihood_all_safe(
       model = model,
       approach = approach,
-      .x,
+      i = i,
       visible_data = assign_folds(visible_data, nfolds),
       degrees,
       non_linear_term = non_linear_term,
@@ -230,11 +294,13 @@ single_cv_all = function(model = "surv",
       sd_initial = sd_initial,
       scale = scale
     )
-  ))
 
-    tibble(fold_output, add_all_degrees_to_fold_likelihood(fold_output, degrees, nfolds)) %>%
-    return()
-}
+    out$result %>% return()
+  }
+
+
+
+
 
 get_fold_likelihood_all = function(model = "surv",
                                     approach = "full",
