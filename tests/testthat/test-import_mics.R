d0a91fa525bf637aa630950ffa76474e3c502546
ajmichaelucd <58995023+ajmichaelucd@users.noreply.github.com>
2024-07-23 17:23:21 -0700

    Small change to first test file

--- tests/testthat/test-import_mics.R
+++ tests/testthat/test-import_mics.R
@@ -1,15 +1,15 @@
 test_that("multiplication works", {
   expected = tibble(
-      left_bound = c(1, 3, -Inf, -Inf),
-      right_bound = c(2, Inf, 1, 1),
-      mic_column = c(4, ">8", "≤2", "=<2"))
+      left_bound = c(1, 3, -Inf, -Inf, NA),
+      right_bound = c(2, Inf, 1, 1, NA),
+      mic_column = c(4, ">8", "≤2", "=<2", NA))
   attr(expected, "source") = "imported"
   attr(expected, "lr_col") = FALSE
   attr(expected, "mic_class") = "imported_mic_column"
   attr(expected, "metadata") = FALSE
   attr(expected, "scale") = "log"
 
-  actual = import_mics(mic_column = c(4, ">8", "≤2", "=<2"))
+  actual = import_mics(mic_column = c(4, ">8", "≤2", "=<2", NA))
   expect_equal(actual, expected)
 })
 
